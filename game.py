64a0f7e42ab2e68ee2a3a65df5024585ebd6ec21
nsardov <nsardov@gmail.com>
2020-05-27 02:51:11 +0300

    mark event msgs ang action msgs

--- game.py
+++ game.py
@@ -12,7 +12,7 @@ def finding_apple() -> None:
     """Увеличивает количество жизни героя на случайное число от 5 до 15."""
     global hero_health
     adding_health = randint(5, 15)
-    print(f'Вы обнаружили волшебное яблоко,'
+    print(f'! Вы обнаружили волшебное яблоко, '
           f'которое увеличивает ваши жизни на {adding_health}!')
     hero_health += adding_health
     print(f'Теперь у вас {hero_health} жизней')
@@ -23,19 +23,19 @@ def finding_sword() -> None:
     """Позволяет игроку изменить силу атаки героя."""
     global hero_strength
     sword_strength = randint(10, 25)
-    print(f'Вы обнаружили новый меч с силой атаки {sword_strength}')
+    print(f'! Вы обнаружили новый меч с силой атаки {sword_strength}')
     answer = 0
     while answer != 1 and answer != 2:
         answer = int(input('Хотите взять новый меч или пройти мимо?\n'
                            '1 - взять\n2 - пройти мимо\n'))
         if answer == 1:
             hero_strength = sword_strength
-            print(f'Вы взяли новый меч,'
+            print(f'=> Вы взяли новый меч, '
                   f'теперь сила вашей атаки {hero_strength}')
         elif answer == 2:
-            print('Вы прошли мимо, сила вашей атаки осталась прежней')
+            print('=> Вы прошли мимо, сила вашей атаки осталась прежней')
         else:
-            print('Вы ввели некорректный ответ, попробуйте ещё раз')
+            print('=> Вы ввели некорректный ответ, попробуйте ещё раз')
     sleep(1)
 
 
@@ -49,18 +49,18 @@ def meeting_monster() -> None:
     monster_health = randint(10, 30)
     answer = 0
     while answer != 1 and answer != 2:
-        answer = int(input(f"Вы встретили монстра с атакой {monster_strength}"
+        answer = int(input(f"! Вы встретили монстра с атакой {monster_strength}"
                            f" и количеством жизней {monster_health}\n"
                            f"Хотите вступить с ним в бой?\n"
                            f"1 - сражаться\n"
                            f"2 - убежать, чтобы набраться сил\n"))
         if answer == 1:
-            print('Вы вступаете в сражение с монстром')
+            print('=> Вы вступаете в сражение с монстром')
             battle(monster_strength, monster_health)
         elif answer == 2:
-            print('Вы убежали, чтобы набраться сил перед следующей встречей')
+            print('=> Вы убежали, чтобы набраться сил перед следующей встречей')
         else:
-            print('Вы ввели некорректный ответ, попробуйте ещё раз')
+            print('=> Вы ввели некорректный ответ, попробуйте ещё раз')
     sleep(1)
 
 
@@ -85,7 +85,7 @@ def battle(monster_strength: int, monster_health: int) -> None:
             print('Вы были убиты, конец игры...')
         elif monster_health <= 0:
             dead_monsters += 1
-            print(f'Вы победили ещё одного монстра!'
+            print(f'Вы победили ещё одного монстра! '
                   f'Общее количество побед: {dead_monsters}\n'
                   f'У вас осталось {hero_health} жизней.')
             if dead_monsters >= 10:
